a0b84c140ead7c03df0cf674ad00ffc4fe19a29e
Ren Peng <renpeng@xiaoice.com>
2022-01-28 11:38:00 +0800

    init

---
+++ setup.py
@@ -0,0 +1,72 @@
+from setuptools import find_packages, setup, Command
+import shutil
+import sys
+import os
+
+import cli
+
+long_description = open("README.md", encoding="utf8").read()
+requirements = [line.strip() for line in open("requirements.txt", encoding="utf8").readlines()]
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            here = os.path.abspath(os.path.dirname(__file__))
+            shutil.rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel'.format(sys.executable))
+
+        self.status('Uploading the package to pypi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(cli.__version__))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+setup(
+    name='cli',
+    version=str(cli.__version__),
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='cli',
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    url='https://github.com/killf/cli',
+    author='killf',
+    cmdclass={
+        'upload': UploadCommand,
+    },
+    entry_points={})
